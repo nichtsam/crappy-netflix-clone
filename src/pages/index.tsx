7034f916a5231b6843b7e4ff4b4ea1529a510588
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-05 01:56:02 +0800

    fix: get comfortable

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p className="text-red-200">Hi!</p>
+      Let's clone Netflix!
+      {/* Header */}
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Footer */}
+      {/* Modal */}
+    </div>
+  );
+};
+
+export default Home;
