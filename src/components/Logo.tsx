046864c6b7a870e58b72c1e31e811b631de9cdfc
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-05 21:30:32 +0800

    fix: prioritize Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,6 +5,7 @@ function Logo() {
   return (
     <div className="relative w-full h-full">
       <Image
+        priority
         src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
         layout="fill"
         objectFit="contain"
