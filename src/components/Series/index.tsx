3b53bc2bc015bd7078c88ba353403e05da784e5e
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-09 03:47:17 +0800

    fix: implement series scroll
    and disable user scroll

--- src/components/Series/index.tsx
+++ src/components/Series/index.tsx
@@ -16,12 +16,34 @@ export interface SeriesProps {
 
 function Series({ title, series }: SeriesProps) {
   const rowRef = useRef<HTMLDivElement>(null);
-  const [hasMoved, setHasMoved] = useState(false);
+  const [canMoveLeft, setCanMoveLeft] = useState(false);
+  const [canMoveRight, setCanMoveRight] = useState(true);
 
   const handleMove = (direction: Direction) => {
     if (!rowRef.current) {
       return;
     }
+
+    const { scrollLeft, clientWidth, scrollWidth } = rowRef.current;
+
+    const scrollTo =
+      direction === Direction.LEFT
+        ? scrollLeft - clientWidth
+        : scrollLeft + clientWidth;
+
+    if (scrollTo > 0) {
+      setCanMoveLeft(true);
+    } else {
+      setCanMoveLeft(false);
+    }
+
+    if (scrollTo + clientWidth < scrollWidth) {
+      setCanMoveRight(true);
+    } else {
+      setCanMoveRight(false);
+    }
+
+    rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
   };
 
   return (
@@ -30,25 +52,31 @@ function Series({ title, series }: SeriesProps) {
         {title}
       </h2>
       <div className="group relative">
-        <ChevronLeftIcon
-          onClick={() => handleMove(Direction.LEFT)}
-          className="z-10 opacity-0 group-hover:opacity-100 h-9 w-9 cursor-pointer absolute top-0 bottom-0 left-2 m-auto hover:scale-125 transition"
-        />
+        {canMoveLeft && (
+          <ChevronLeftIcon
+            onClick={() => handleMove(Direction.LEFT)}
+            className="z-10 opacity-0 group-hover:opacity-100 h-9 w-9 cursor-pointer absolute top-0 bottom-0 left-2 m-auto hover:scale-125 transition"
+          />
+        )}
 
         {/* tailwind space uses margin but netflix does it with padding, hence a cut in layout */}
-        <div
-          ref={rowRef}
-          className="whitespace-nowrap space-x-[0.4vw] text-[0]"
-        >
-          {series.map((movie) => (
-            <Thumbnail key={movie.id} movie={movie} />
-          ))}
+        <div ref={rowRef} className="overflow-scroll scrollbar-hide">
+          <div className="whitespace-nowrap space-x-[0.4vw] text-[0]">
+            {series.map((movie) => (
+              <Thumbnail key={movie.id} movie={movie} />
+            ))}
+          </div>
         </div>
 
-        <ChevronRightIcon
-          onClick={() => handleMove(Direction.RIGHT)}
-          className="z-10 opacity-0 group-hover:opacity-100 h-9 w-9 cursor-pointer absolute top-0 bottom-0 right-2 m-auto hover:scale-125 transition"
-        />
+        {/* disable user scroll */}
+        <div className="absolute-full"></div>
+
+        {canMoveRight && (
+          <ChevronRightIcon
+            onClick={() => handleMove(Direction.RIGHT)}
+            className="z-10 opacity-0 group-hover:opacity-100 h-9 w-9 cursor-pointer absolute top-0 bottom-0 right-2 m-auto hover:scale-125 transition"
+          />
+        )}
       </div>
     </div>
   );
