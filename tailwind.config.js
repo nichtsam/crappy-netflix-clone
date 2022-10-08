4b254b2d3772e5c2707e660e68ef6b744b212f17
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-08 21:32:41 +0800

    feat: add scorll-hide tailwind utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        ".scrollbar-hide": {
+          /* IE and Edge */
+          "-ms-overflow-style": "none",
+
+          /* Firefox */
+          "scrollbar-width": "none",
+
+          /* Safari and Chrome */
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        },
+      });
+    },
+  ],
 };
