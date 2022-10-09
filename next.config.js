7fd1a8ba351926ee662b8a0778c940dab72ea8f2
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-10-09 19:04:21 +0800

    fix: redirect "/" to "/browse"

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ module.exports = {
   images: {
     domains: ["upload.wikimedia.org", "image.tmdb.org"],
   },
+  redirects: () => [
+    {
+      source: "/",
+      destination: "/browse",
+      permanent: false,
+    },
+  ],
   experimental: {
     newNextLinkBehavior: true,
   },
