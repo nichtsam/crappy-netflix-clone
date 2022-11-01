81dedb10dec99343005fecd49a2a40b2809cd311
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2022-11-01 23:06:29 +0800

    feat: login validation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,16 +5,29 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface ILoginInput {
-  id: string | undefined;
-  password: string | undefined;
-}
+const loginSchema = z.object({
+  id: z.string().email("Please enter a valid email."),
+  password: z
+    .string()
+    .min(4, "Your password must contain between 4 and 60 characters.")
+    .max(60, "Your password must contain between 4 and 60 characters."),
+});
+
+type LoginInput = z.infer<typeof loginSchema>;
 
 const Login: NextPage = () => {
-  const { register, handleSubmit } = useForm<ILoginInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+  });
 
-  const onSubmit: SubmitHandler<ILoginInput> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<LoginInput> = (data) => console.log(data);
 
   return (
     <div className="min-h-screen before:h-24 before:block after:h-60 after:block relative">
@@ -37,50 +50,75 @@ const Login: NextPage = () => {
       </header>
 
       <div className="px-[5%] md:px-0 bg-black/75 md:mx-auto md:max-w-md">
-        <div className="md:p-14">
+        <div className="md:p-14 min-h-[550px] flex flex-col">
           <h1 className="mb-7 text-3xl">Sign In</h1>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="relative mb-4">
-              <input
-                id="loginId"
-                {...register("id")}
-                placeholder=" "
-                className={clsx(
-                  "peer rounded px-5 pt-4 h-12 w-full bg-neutral-700 focus:bg-neutral-600 outline-none"
-                )}
-              ></input>
-              <label
-                htmlFor="loginId"
-                className={clsx(
-                  "absolute top-1/2 -translate-y-1/2 left-5 text-neutral-400 transition-all ease-out",
-                  "peer-focus:translate-y-0 peer-focus:top-2 peer-focus:text-xs",
-                  "peer-[:not(:placeholder-shown)]:translate-y-0 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs"
-                )}
+          <form className="flex-1" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4">
+              <div
+                className={clsx("relative", {
+                  "after:h-2 after:border-b-amber-500 after:border-b-2 after:absolute after:bottom-0 after:left-0 after:w-full after:bg-transparent after:rounded":
+                    !!errors.id,
+                })}
               >
-                Email or phone number
-              </label>
+                <input
+                  id="loginId"
+                  {...register("id")}
+                  placeholder=" "
+                  className={clsx(
+                    "peer rounded px-5 pt-4 h-12 w-full bg-neutral-700 focus:bg-neutral-600 outline-none"
+                  )}
+                ></input>
+                <label
+                  htmlFor="loginId"
+                  className={clsx(
+                    "absolute top-1/2 -translate-y-1/2 left-5 text-neutral-400 transition-all ease-out",
+                    "peer-focus:translate-y-0 peer-focus:top-2 peer-focus:text-xs",
+                    "peer-[:not(:placeholder-shown)]:translate-y-0 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs"
+                  )}
+                >
+                  Email
+                </label>
+              </div>
+              {errors.id && (
+                <span className="text-xs text-amber-500">
+                  {errors.id.message}
+                </span>
+              )}
             </div>
 
-            <div className="relative mb-4">
-              <input
-                id="loginPassword"
-                {...register("password")}
-                placeholder=" "
-                className={clsx(
-                  "peer rounded px-5 pt-4 h-12 w-full bg-neutral-700 focus:bg-neutral-600 outline-none"
-                )}
-              ></input>
-              <label
-                htmlFor="loginPassword"
-                className={clsx(
-                  "absolute top-1/2 -translate-y-1/2 left-5 text-neutral-400 transition-all ease-out",
-                  "peer-focus:translate-y-0 peer-focus:top-2 peer-focus:text-xs",
-                  "peer-[:not(:placeholder-shown)]:translate-y-0 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs"
-                )}
+            <div className="mb-4">
+              <div
+                className={clsx("relative", {
+                  "after:h-2 after:border-b-amber-500 after:border-b-2 after:absolute after:bottom-0 after:left-0 after:w-full after:bg-transparent after:rounded":
+                    !!errors.id,
+                })}
               >
-                Password
-              </label>
+                <input
+                  id="loginPassword"
+                  type="password"
+                  {...register("password")}
+                  placeholder=" "
+                  className={clsx(
+                    "peer rounded px-5 pt-4 h-12 w-full bg-neutral-700 focus:bg-neutral-600 outline-none"
+                  )}
+                ></input>
+                <label
+                  htmlFor="loginPassword"
+                  className={clsx(
+                    "absolute top-1/2 -translate-y-1/2 left-5 text-neutral-400 transition-all ease-out",
+                    "peer-focus:translate-y-0 peer-focus:top-2 peer-focus:text-xs",
+                    "peer-[:not(:placeholder-shown)]:translate-y-0 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs"
+                  )}
+                >
+                  Password
+                </label>
+              </div>
+              {errors.password && (
+                <span className="text-xs text-amber-500">
+                  {errors.password.message}
+                </span>
+              )}
             </div>
 
             <button
@@ -91,7 +129,7 @@ const Login: NextPage = () => {
             </button>
           </form>
 
-          <p className="text-neutral-400 mt-24">
+          <p className="text-neutral-400 mt-4">
             New to Netflix?{" "}
             <Link href="#" className="text-white">
               Sign up now
